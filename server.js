002b034e7468ded20f4b0da5c2eda6d92615ed6d
priti maurya <lanetteam.priti@gmail.com>
2018-02-05 12:53:04 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+express = require('express')
+hbs = require('hbs')
+
+app = express();
+app.use(express.static(__dirname + '/public'))
+
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname + '/views/partials')
+
+hbs.registerHelper('year',()=>{
+    return new Date().getFullYear()
+});
+
+
+hbs.registerHelper('toUpper',(msg)=>{
+    return msg.toUpperCase();
+});
+
+// app.use((req,res,next)=>{
+//     res.render('maintenance.hbs');
+// })
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        title: 'home Page',
+        message: 'Welcome to my site'
+    })
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        title: 'about Page',
+        message:'This is About page'
+    })
+})
+
+app.listen(3001,()=>{});
